def38e8f35ac7e692b7a6024cb56f60268db00cf
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-11-07 15:35:31 +0100

    Initialize project

---
+++ hospital-equipment/src/main/java/com/ISAproject/hospitalequipment/HospitalEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.ISAproject.hospitalequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalEquipmentApplication.class, args);
+	}
+
+}
---
+++ hospital-equipment/src/test/java/com/ISAproject/hospitalequipment/HospitalEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISAproject.hospitalequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
