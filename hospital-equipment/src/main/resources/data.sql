c99780b8cb4e07f27915864993a8aa5d7634d2a4
Ninoli1 <116906127+Ninoli1@users.noreply.github.com>
2023-11-19 21:02:43 +0100

    update : sql for my task

--- hospital-equipment/src/main/resources/data.sql
+++ hospital-equipment/src/main/resources/data.sql
@@ -9,6 +9,17 @@ VALUES ( 'Belgrad', 'Serbia', '45', 'Knez Mihailova');
 INSERT INTO addresses(
      city, country, number, street)
 VALUES ( 'Nis', 'Serbia', '60', 'Mileve Maric');
+INSERT INTO addresses(
+     city, country, number, street)
+VALUES ( 'Trebinje', 'Bih', '60', 'Nemanjina');
+
+INSERT INTO addresses(
+     city, country, number, street)
+VALUES ( 'Bijeljina', 'Bih', '50', 'Vozda Karadjordja');
+
+INSERT INTO addresses(
+     city, country, number, street)
+VALUES ( 'Kragujevac', 'Serbia', '10', 'Mileve Maric');
 
 
 INSERT INTO role(
@@ -35,6 +46,17 @@ VALUES ( 'example@gmail.com', false, 'Milos', 'Milosevic', 'doctor', '2670', '06
 INSERT INTO users(
      email, enabled, first_name, last_name, occupation, password, phone_number, address_id)
 VALUES ( 'nikolinaskiljevic@gmail.com',false, 'Nikolina', 'Skiljevic', 'nurse', '36782',  '069-3782-738', 3);
+INSERT INTO users(
+     email, enabled, first_name, last_name, occupation, password, phone_number, address_id)
+VALUES ( 'janko@gmail.com',false, 'Janko', 'Jankovic', 'nurse', '36782',  '069-3782-738', 4);
+
+INSERT INTO users(
+     email, enabled, first_name, last_name, occupation, password, phone_number, address_id)
+VALUES ( 'marko@gmail.com',false, 'Marko', 'Markovic', 'nurse', '222345',  '069-3782-738', 5);
+
+INSERT INTO users(
+     email, enabled, first_name, last_name, occupation, password, phone_number, address_id)
+VALUES ( 'Mirko@gmail.com',false, 'Mirko', 'Mirkovic', 'nurse', '36782',  '069-3782-738', 6);
 
 
 
@@ -49,6 +71,17 @@ VALUES (1, 2);
 INSERT INTO user_role(
     role_id, user_id)
 VALUES (2, 3);
+INSERT INTO user_role(
+    role_id, user_id)
+VALUES (2, 4);
+
+INSERT INTO user_role(
+    role_id, user_id)
+VALUES (2, 5);
+
+INSERT INTO user_role(
+    role_id, user_id)
+VALUES (2, 6);
 
 
 INSERT INTO loyalty_program(
@@ -75,3 +108,87 @@ VALUES (100, 2, 2,'GOLD');
 INSERT INTO registered_users(
     penalty_points, id,loyalty_id, user_category)
 VALUES (27, 3,3, 'SILVER');
+
+INSERT INTO company_profiles(
+     name, description, grade, address_id)
+VALUES ( 'Corpore Sano', 'The best service', 5, 1);
+
+INSERT INTO company_profiles(
+     name, description, grade, address_id)
+VALUES ( 'Health Company', 'Safe with us', 4, 2);
+
+INSERT INTO company_profiles(
+     name, description, grade, address_id)
+VALUES ( 'HealthGroup', 'Best service', 4, 3);
+
+
+
+INSERT INTO equipments(
+     amount, grade, description, name)
+VALUES ( 50, 5, 'Strong', 'Scissors');
+
+INSERT INTO equipments(
+     amount, grade, description, name)
+VALUES ( 2000, 4, 'Medical', 'Gloves');
+
+INSERT INTO equipments(
+     amount, grade, description, name)
+VALUES ( 500, 5, 'Cotton', 'Coat');
+
+INSERT INTO equipments(
+     amount, grade, description, name)
+VALUES ( 500, 5, 'elastic', 'Bandage');
+
+
+
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 1,1);
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 1,2);
+
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 2,2);
+
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 2,3);
+
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 3,1);
+INSERT INTO company_equipment(
+     company_profile_id, equipment_id)
+VALUES ( 3,3);
+
+
+INSERT INTO appointments(
+     duration, company_profile_id, date, admin_last_name, admin_name)
+VALUES ( 22, 1,'2023-10-22 21:48:24.891+02','Nina','Nina');
+INSERT INTO appointments(
+     duration, company_profile_id, date, admin_last_name, admin_name)
+VALUES ( 10, 1,'2023-10-22 21:48:24.891+02','Ana','Ana');
+INSERT INTO appointments(
+     duration, company_profile_id, date, admin_last_name, admin_name)
+VALUES ( 30, 2,'2023-10-22 21:48:24.891+02','Helena','Helena');
+INSERT INTO appointments(
+     duration, company_profile_id, date, admin_last_name, admin_name)
+VALUES ( 22, 3,'2023-10-22 21:48:24.891+02','Dajana','Dajana');
+INSERT INTO appointments(
+     duration, company_profile_id, date, admin_last_name, admin_name)
+VALUES ( 2, 3,'2023-10-22 21:48:24.891+02','Katarina','Katarina');
+
+INSERT INTO company_administrators(
+     company_profile_id, id)
+VALUES ( 1,3);
+INSERT INTO company_administrators(
+     company_profile_id, id)
+VALUES ( 1,6);
+INSERT INTO company_administrators(
+     company_profile_id, id)
+VALUES ( 2,4);
+INSERT INTO company_administrators(
+     company_profile_id, id)
+VALUES ( 3,5);
